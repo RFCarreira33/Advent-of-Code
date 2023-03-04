19d3c4781b9ba6585b4625795188be1a259ec53a
Rodrigoporextenso <2213146@my.ipleiria.pt>
2023-03-04 16:19:43 +0000

    added day 2

---
+++ 2021/day2/src/main.rs
@@ -0,0 +1,55 @@
+use std::fs;
+
+fn parse_input() -> Vec<String> {
+    return fs::read_to_string("input.txt")
+        .expect("Unable to read file")
+        .split_whitespace()
+        .map(|s| s.to_string())
+        .collect();
+}
+
+fn part1(input: Vec<String>) -> i32 {
+    let mut depth: i32 = 0;
+    let mut horizontal: i32 = 0;
+    for i in (0..input.len()).step_by(2) {
+        let direction: &str = &input[i];
+        let value: i32 = input[i + 1].parse::<i32>().unwrap();
+        // depending on the command the submarine moves
+        match direction {
+            "forward" => horizontal += value,
+            "up" => depth -= value,
+            "down" => depth += value,
+            _ => eprintln!("Error"),
+        }
+    }
+
+    return depth * horizontal;
+}
+
+fn part2(input: Vec<String>) -> i32 {
+    let mut aim: i32 = 0;
+    let mut depth: i32 = 0;
+    let mut horizontal: i32 = 0;
+    for i in (0..input.len()).step_by(2) {
+        let direction: &str = &input[i];
+        let value: i32 = input[i + 1].parse::<i32>().unwrap();
+        // depending on the command the submarine moves
+        match direction {
+            "forward" => {
+                horizontal += value;
+                depth += aim * value;
+            }
+            "up" => aim -= value,
+            "down" => aim += value,
+            _ => eprintln!("Error"),
+        }
+    }
+
+    return depth * horizontal;
+}
+
+fn main() {
+    let input: Vec<String> = parse_input();
+    // println!("Part 1 = {}\n", part1(input));
+    println!("Part 2 = {}\n", part2(input));
+}
--- README.md
+++ README.md
@@ -3,3 +3,4 @@
 I started with the year 2022 and I plan on solving the previous year's challenges to learn new languages and to stop my brain from turning into dust.
 
 - 2022: Solved in C++, up to day 15
+- 2021: Solved in Rust, up to day 2
